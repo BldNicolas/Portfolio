0965ad7f8349cb2442ff9f5195b0c48fcf7dd272
Nicolas <ni.berlaud@gmail.com>
2024-02-08 23:34:09 +0100

    💄 Add tns() slider

---
+++ js/tiny-slider.js
@@ -0,0 +1,31 @@
+var slider = tns({
+    container: ".slider-1",
+    items: 3,
+    speed: 500,
+    mouseDrag: true,
+    autoplay: true,
+    autoplayButtonOutput: false,
+    center: true,
+    loop: false,
+    nav: false,
+    rewind: true,
+    controlsContainer: "#custom-control-1",
+    controlsPosition: "bottom",
+    startIndex: 1
+});
+
+var slider = tns({
+    container: ".slider-2",
+    items: 3,
+    speed: 500,
+    mouseDrag: true,
+    autoplay: true,
+    autoplayButtonOutput: false,
+    center: true,
+    loop: false,
+    nav: false,
+    rewind: true,
+    controlsContainer: "#custom-control-2",
+    controlsPosition: "bottom",
+    startIndex: 2
+});
